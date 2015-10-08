8500fe2fbf4f18dcd9ebaea7cc1900d5c0728fd5
Kris Orr <kris.orr@shopify.com>
2015-10-08 10:15:11 -0400

    add missing copyright headers

--- MobileBuy/buy/src/main/java/com/shopify/buy/ui/ProductDetailsConstants.java
+++ MobileBuy/buy/src/main/java/com/shopify/buy/ui/ProductDetailsConstants.java
@@ -24,16 +24,10 @@
 
 package com.shopify.buy.ui;
 
+import com.shopify.buy.ui.common.BaseConstants;
+
 /**
- * Common constants used by the package.
+ * Common constants used by the {@link ProductDetailsActivity}.
  */
-class ProductDetailsConstants {
-
-    public static final String EXTRA_ERROR_CODE = "com.shopify.buy.ui.ERROR_CODE";
-    public static final String EXTRA_ERROR_MESSAGE = "com.shopify.buy.ui.ERROR_MESSAGE";
-    public static final String EXTRA_CHECKOUT = "com.shopify.buy.ui.CHECKOUT";
-
-    public static final int ERROR_GET_SHOP_FAILED = 1;
-    public static final int ERROR_GET_PRODUCT_FAILED = 2;
-
-}
+@Deprecated
+class ProductDetailsConstants extends BaseConstants {}
